f29cb05ef7494b391c0fa4c3eee049c9ae44ac11
DenisKukushkin <denisoverhere@gmail.com>
2022-11-11 14:27:41 +0300

    Initial Commit

---
+++ RGBSwiftUI/RGBSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RGBSwiftUIApp.swift
+//  RGBSwiftUI
+//
+//  Created by Кукушкин Денис Сергеевич on 11.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct RGBSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
